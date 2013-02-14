95dde7be9e011f6370c792bad8fb5ea6f3bacdf6
Josiah Baldwin <jbaldwin8889@gmail.com>
2013-02-14 16:24:17 -0600

    Added dependency checking to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from distutils.core import setup
+try:
+    from gi.repository import Gtk, Gdk, GObject
 
-setup(
-    name="notes",
-    version='0.1',
-    scripts=["bin/notes"]
-)
+    setup(
+        name="notes",
+        version='0.1',
+        scripts=["bin/notes"]
+    )
+except:
+    print "GTK3/Gobject introspection requirements not met, please install GTK3/GObject introspection before installing this program."
